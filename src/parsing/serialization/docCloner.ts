4e0e1f8d4ef86875973334ca384e95cbff769266
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-11-01 11:22:39 -0600

    docCloner messy

--- src/parsing/serialization/docCloner.ts
+++ src/parsing/serialization/docCloner.ts
@@ -2,6 +2,7 @@ import { MEDIA_RULE_TYPE, STYLE_RULE_TYPE } from "../../index.types";
 import {
   DocClone,
   MediaRuleClone,
+  RuleClone,
   SheetClone,
   StyleRuleClone,
 } from "./docClone";
@@ -27,51 +28,58 @@ function cloneDoc(doc: Document, ctx: CloneDocContext): DocClone {
 
   for (const sheet of accessibleSheets) {
     const sheetClone = new SheetClone(ctx);
-    for (const rule of Array.from(sheet.cssRules)) {
-      if (rule.type === STYLE_RULE_TYPE) {
-        const styleRule = rule as CSSStyleRule;
-        const styleRuleClone = new StyleRuleClone(ctx);
-        styleRuleClone.selector = styleRule.selectorText;
-
-        const style: Record<string, string> = {};
-        const specialProps: Record<string, string> = {};
-
-        for (let i = 0; i < styleRule.style.length; i++) {
-          const prop = styleRule.style[i];
-
-          if (FLUID_PROPERTY_NAMES.has(prop)) {
-            const shorthandMap = SHORTHAND_PROPERTIES[prop];
-            if (shorthandMap) {
-              if (isBrowser) continue;
-
-              ///TODO: Expand shorthands
-              continue;
-            }
-            style[prop] = styleRule.style.getPropertyValue(prop);
-          } else if (SPECIAL_PROPERTIES.has(prop)) {
-            specialProps[prop] = styleRule.style.getPropertyValue(prop);
-          }
-        }
-
-        styleRuleClone.style = style;
-        styleRuleClone.specialProperties = specialProps;
-        sheetClone.rules.push(styleRuleClone);
-      } else if (rule.type === MEDIA_RULE_TYPE) {
-        const mediaRule = rule as CSSMediaRule;
-        const mediaRuleClone = new MediaRuleClone(ctx);
-
-        const match = mediaRule.media.mediaText.match(
-          /\(min-width:\s*(\d+)px\)/
-        );
-        if (match) {
-          mediaRuleClone.minWidth = Number(match[1]);
-          mediaRuleClone.rules = cloneRules(mediaRule.cssRules, ctx);
-        }
-        sheetClone.rules.push(mediaRuleClone);
-      }
-    }
+    sheetClone.rules = cloneRules(sheet.cssRules, ctx);
     docClone.sheets.push(sheetClone);
   }
 
   return docClone;
+}
+
+function cloneRules(rules: CSSRuleList, ctx: CloneDocContext): RuleClone[] {
+  const { isBrowser } = ctx;
+  const result: RuleClone[] = [];
+  for (const rule of Array.from(rules)) {
+    if (rule.type === STYLE_RULE_TYPE) {
+      const styleRule = rule as CSSStyleRule;
+      const styleRuleClone = new StyleRuleClone(ctx);
+      styleRuleClone.selector = styleRule.selectorText;
+
+      const style: Record<string, string> = {};
+      const specialProps: Record<string, string> = {};
+
+      for (let i = 0; i < styleRule.style.length; i++) {
+        const prop = styleRule.style[i];
+
+        if (FLUID_PROPERTY_NAMES.has(prop)) {
+          const shorthandMap = SHORTHAND_PROPERTIES[prop];
+          if (shorthandMap) {
+            if (isBrowser) continue;
+
+            ///TODO: Expand shorthands
+            continue;
+          }
+          style[prop] = styleRule.style.getPropertyValue(prop);
+        } else if (SPECIAL_PROPERTIES.has(prop)) {
+          specialProps[prop] = styleRule.style.getPropertyValue(prop);
+        }
+      }
+
+      styleRuleClone.style = style;
+      styleRuleClone.specialProperties = specialProps;
+      result.push(styleRuleClone);
+    } else if (rule.type === MEDIA_RULE_TYPE) {
+      const mediaRule = rule as CSSMediaRule;
+      const mediaRuleClone = new MediaRuleClone(ctx);
+
+      const match = mediaRule.media.mediaText.match(/\(min-width:\s*(\d+)px\)/);
+      if (match) {
+        mediaRuleClone.minWidth = Number(match[1]);
+        mediaRuleClone.rules = cloneRules(mediaRule.cssRules, ctx).filter(
+          (rule) => rule.type === STYLE_RULE_TYPE
+        ) as StyleRuleClone[];
+      }
+      result.push(mediaRuleClone);
+    }
+  }
+  return result;
 }
