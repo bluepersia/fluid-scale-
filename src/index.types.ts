25d9ac38c261edce1458f16b6e865a5de6bece1c
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-11-13 12:16:57 -0600

    Removed event context

--- src/index.types.ts
+++ src/index.types.ts
@@ -1,5 +1,3 @@
-import type { EventBus } from "gold-sight";
-
 type Global = UserSettings & Features & {};
 
 type UserSettings = {
@@ -10,13 +8,7 @@ type Features = {
   isBrowser: boolean;
 };
 
-type EventContext = {
-  event?: EventBus;
-  eventUUID?: string;
-  eventUUIDs?: string[];
-};
-
 const STYLE_RULE_TYPE = 1;
 const MEDIA_RULE_TYPE = 4;
 export { STYLE_RULE_TYPE, MEDIA_RULE_TYPE };
-export type { Global, UserSettings, Features, EventContext };
+export type { Global, UserSettings, Features };
